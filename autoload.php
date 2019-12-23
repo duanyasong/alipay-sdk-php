ff8dda6b7b072df3b9a0fa237f396d1283c70210
duanyasong <mark.duan@silksoftware.com>
2019-12-23 16:43:04 +0800

    #modified:   autoload.php

--- autoload.php
+++ autoload.php
@@ -1,14 +1,13 @@
 <?php
     // Now that we have an autoloader, let's register it!
     spl_autoload_register(function($class){
-		$file = __DIR__ . 'src/'.$class.'.php';
+		$file = __DIR__ . '/src/aop/'.$class.'.php';
+
 		if(file_exists($file)){
 			require_once($file);
 		}
-		$file = __DIR__ . 'src/request/'.$class.'.php';
+		$file = __DIR__ . 'src/aop/request/'.$class.'.php';
 		if(file_exists($file)){
 			require_once($file);
 		}
 	});
-
-}
